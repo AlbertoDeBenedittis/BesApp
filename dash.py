9ff2eb9e92d603b9723b53507ac2a6ab8374a702
AlbertoDeBenedittis <alberto.debenedittis@studenti.unitn.it>
2022-04-25 16:29:07 +0200

    Add select box for the year

--- dash.py
+++ dash.py
@@ -100,7 +100,10 @@ selectbox_area = st.sidebar.selectbox(
     ('Macros', 'Region', 'Provinces')
 )
 
-selectbox_y = '2018'
+selectbox_y = st.sidebar.selectbox(
+    'Year of analysis',
+    ('2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019')
+)
 selectbox_type = st.sidebar.selectbox('Which type of plot do you want to visualize?', 
 ('Static', 'Dynamic'))
 
